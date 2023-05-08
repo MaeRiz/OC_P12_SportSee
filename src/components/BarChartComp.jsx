71f24ece062d148b45808911833966d8f5741c3e
Mahery Gonin <mahery.gonin@gmail.com>
2023-05-08 17:03:27 +0200

    Add barchart comp

---
+++ src/components/BarChartComp.jsx
@@ -0,0 +1,81 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
+import PropTypes from "prop-types";
+
+const BarChartComp = ({ datas }) => {
+  return (
+    <div className="barchart__container">
+      <div className="barchart__header">
+        <p className="barchart__header--title">Activité quotidienne</p>
+        <ul className="barchart__header--legend">
+          <li className="barchart__header--legend--text-kg">
+            <i className="fa-solid fa-circle"></i> Poids (kg)
+          </li>
+          <li className="barchart__header--legend--text-cal">
+            <i className="fa-solid fa-circle"></i> Calories (kCal)
+          </li>
+        </ul>
+      </div>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={datas}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="2" vertical={false} />
+
+          <XAxis tickLine={false} axisLine={false} />
+          <YAxis orientation="right" tickLine={false} axisLine={false} />
+          <Tooltip
+            content={<CustomTooltip />}
+            wrapperStyle={{ outline: "none" }}
+          />
+          <Bar
+            dataKey="kilogram"
+            fill="#282D30"
+            barSize={8}
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="calories"
+            fill="#E60000"
+            barSize={8}
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+BarChartComp.propTypes = {
+  datas: PropTypes.array,
+};
+
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="custom-tooltip--text">
+          {payload[0].payload.calories} kCal
+        </p>
+        <p className="custom-tooltip--text">{payload[0].payload.kilogram} kg</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default BarChartComp;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,33 @@
 import { useParams } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import getUserDatas from "../utils/getUserDatas";
+import BarChartComp from "../components/BarChartComp";
 
 const Dashboard = () => {
   let { userId } = useParams();
 
   const user = new getUserDatas("useMock");
-  //const user = new getUserDatas(userId);
+  // const user = new getUserDatas(userId);
 
   return (
     <div id="dashboard">
       <SideBar />
+      <div id="page_content">
+        <div className="welcome">
+          <h3 className="welcome--title">
+            Bonjour{" "}
+            <span className="welcome--title-name">
+              {user.userInfos.data.userInfos.firstName}
+            </span>
+          </h3>
+          <p className="welcome--goodjob">
+            Félicitation ! Vous avez explosé vos objectifs hier 👏
+          </p>
+        </div>
+        <div className="charts__container">
+          <BarChartComp datas={user.activity.data.sessions} />
+        </div>
+      </div>
     </div>
   );
 };
